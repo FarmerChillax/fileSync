0b42860ee2c2bfd59148090273a9b186e95653e2
Farmer.Chillax <farmer-chong@qq.com>
2021-12-09 19:36:40 +0800

    docs: 添加注释

--- core/error.go
+++ core/error.go
@@ -2,6 +2,8 @@ package core
 
 import "log"
 
+// 处理错误信息
+// message: 消息前缀, err: error体
 func HandleError(message string, err error) bool {
 	if err != nil {
 		log.Printf("[%s], err: %v\n", message, err)
